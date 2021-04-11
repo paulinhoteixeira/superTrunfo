b09af8660a8f8e15126e92f3ad57da6eb8385dbd
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-04-11 19:43:22 -0300

    adicionadas mais cartas ao jogo

--- script.js
+++ script.js
@@ -1,35 +1,82 @@
-let cartaPaulo = {
+var cartaPaulo = {
   nome: "Seiya de Pegaso",
-  imagem:
-    "https://i.pinimg.com/originals/e9/c1/77/e9c1778727e6c604ace92a2ec73ce52b.jpg",
+  imagem: "https://i.pinimg.com/originals/c2/1a/ac/c21aacd5d092bf17cfff269091f04606.jpg",
   atributos: {
-    ataque: 80,
-    defesa: 60,
-    magia: 90,
-  },
-};
+      ataque: 80,
+      defesa: 60,
+      magia: 90
+  }
+}
 
-let cartaRafa = {
+var cartaRafa = {
   nome: "Bulbasauro",
-  imagem:
-    "http://4.bp.blogspot.com/-ZoCqleSAYNc/UQgfMdobjUI/AAAAAAAACP0/s_iiWjmw2Ys/s1600/001Bulbasaur_Dream.png",
+  imagem: "http://4.bp.blogspot.com/-ZoCqleSAYNc/UQgfMdobjUI/AAAAAAAACP0/s_iiWjmw2Ys/s1600/001Bulbasaur_Dream.png",
   atributos: {
-    ataque: 70,
-    defesa: 65,
-    magia: 85,
-  },
-};
+      ataque: 70,
+      defesa: 65,
+      magia: 85
+  }
+}
 
-let cartaGuilherme = {
+var cartaGui = {
   nome: "Lorde Darth Vader",
-  imagem:
-    "https://images-na.ssl-images-amazon.com/images/I/51VJBqMZVAL._SX328_BO1,204,203,200_.jpg",
+  imagem: "https://images-na.ssl-images-amazon.com/images/I/51VJBqMZVAL._SX328_BO1,204,203,200_.jpg",
   atributos: {
-    ataque: 88,
-    defesa: 62,
-    magia: 90,
-  },
-};
+      ataque: 88,
+      defesa: 62,
+      magia: 90
+  }
+}
+
+var cartaLol = {
+  nome: "Caitlyn",
+  imagem: "http://1.bp.blogspot.com/-K7CbqWc1-p0/VLc98v85s0I/AAAAAAAABqk/-ZB684VVHbg/s1600/Caitlyn_OriginalSkin.jpg",
+  atributos: {
+      ataque: 95,
+      defesa: 40,
+      magia: 10
+  }
+}
+
+var cartaNaruto = {
+  nome: "Naruto",
+  imagem: "https://conteudo.imguol.com.br/c/entretenimento/16/2017/06/27/naruto-1498593686428_v2_450x337.png",
+  atributos: {
+      ataque: 80,
+      defesa: 60,
+      magia: 100
+  }
+}
+
+var cartaHarry = {
+  nome: "Harry Potter",
+  imagem: "https://sm.ign.com/ign_br/screenshot/default/89ff10dd-aa41-4d17-ae8f-835281ebd3fd_49hp.jpg",
+  atributos: {
+      ataque: 70,
+      defesa: 50,
+      magia: 95
+  }
+}
+
+var cartaBatman = {
+  nome: "Batman",
+  imagem: "https://assets.b9.com.br/wp-content/uploads/2020/09/Batman-issue86-heder-1280x677.jpg",
+  atributos: {
+      ataque: 95,
+      defesa: 70,
+      magia: 0
+  }
+}
+
+var cartaMarvel = {
+  nome: "Capitã Marvel",
+  imagem: "https://cinepop.com.br/wp-content/uploads/2018/09/capitamarvel21.jpg",
+  atributos: {
+      ataque: 90,
+      defesa: 80,
+      magia: 0
+  }
+}
 
 var cartaMaquina;
 var cartaJogador;
